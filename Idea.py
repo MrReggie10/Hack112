12d512c983ced7578088df8dbc862b11c2953c18
joshuak3 <jckimm6306@gmail.com>
2024-11-15 18:40:16 -0500

    actually idead

---
+++ Idea.py
@@ -0,0 +1,18 @@
+#Spell-casting game
+#   use OpenCV to track object
+#       maybe pencil or hand (as wand)
+#       OR RUBBER DUCK
+#   enemies
+#       damage dealt scales depending on how accurate the spell is drawn
+#   list of spells
+#       randomly chooses spell to cast
+#       possible: different spells are more effective against different enemies
+#                       funny spells (flat tire)
+#                 different types of enemies: assertionError, syntaxError, ground, flying, vehicles
+#   use hogwarts picture for background
+
+#   JOBS:
+#   Jacob - OpenCV, tracking duck
+#   Valerio - checking if mouse and spell overlap
+#   Bjorn - Damage system
+#   Josh - spell design/enemy design

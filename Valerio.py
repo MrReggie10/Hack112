7ad54375152d62b653f4ce82e23f62852e5dcb05
vgomezfr <vgomezfr@andrew.cmu.edu>
2024-11-15 20:35:44 -0500

    Removed fluff

--- Valerio.py
+++ Valerio.py
@@ -10,24 +10,23 @@ from types import SimpleNamespace
 def onAppStart(app):
     app.width = 800
     app.height = 600
-    app.cx, app.cy = 200, 200
     app.spellList = ['circle']
-    app.currentSpell = chooseSpell(app)
-    app.error = 0
-    app.path = []
-    app.fun = True
+    reset(app)
     
+    
+def reset(app):
+    app.cx, app.cy = 200, 200
+    app.currentSpell = chooseSpell(app)
+    app.path = []
 
 def redrawAll(app):
     drawCircle(app.cx, app.cy, 15, fill='blue')
     drawSpell(app)
-    drawLabel(f'{app.error}', app.width/6, app.height/6)
     for x, y in app.path:
         drawCircle(x, y, 15, fill='blue', opacity=15)
 
 def onMouseMove(app, mouseX, mouseY):
     app.cx, app.cy = mouseX, mouseY
-    # app.error = calculateError(app)
     app.path.append((mouseX, mouseY))
 
 
@@ -39,11 +38,6 @@ def drawSpell(app):
     if app.currentSpell == 'circle':
         drawCircle(app.width/2, app.height/2, 100, fill=None, border='red')
         drawLabel('Expelliarmus!', app.width/2, app.height/2, size=16, fill='red')
-
-def calculateError(app):
-    if app.currentSpell == 'circle':
-        dist = distance(app.cx, app.cy, app.width/2, app.height/2)
-        return (dist - 100) / 100
 
 def distance(x0, y0, x1, y1):
     return ((x1-x0)**2 + (y1 - y0)**2)**0.5

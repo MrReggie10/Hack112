662226ffc37d5bded60ec6a563978a89a70753b9
Reggie <jacobdyak@gmail.com>
2024-11-15 22:30:00 -0500

    Duck Position draws on cmu_graphics

--- Jacobtesting.py
+++ Jacobtesting.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
+from cmu_graphics import *
 
-camera = cv2.VideoCapture(0)
-
+# Michael Reeves: https://www.youtube.com/watch?v=USKD3vPD6ZA&t=726s
 def generateColorMask(img):
     lowerBound = np.array([10, 210, 190])
     upperBound = np.array([30, 255, 255])
@@ -25,7 +25,7 @@ def computeAveragePosition(img):
         avgY //= counter
     return avgX, avgY
 
-while True:
+def capturePosition():
     ret, frame = camera.read()
     frameReversed = frame[:, ::-1]
     mask = generateColorMask(frameReversed)
@@ -33,12 +33,22 @@ while True:
 
     x, y = computeAveragePosition(mask)
 
-    pos = (x, y, int(np.mean(mask, (0, 1)) * 50))
+    pos = (x * 800 // (mask[0].size), y * 450 // (mask[:,0].size), int(np.mean(mask, (0, 1)) * 50))
 
-    print(pos)
+    return pos
 
-    if cv2.waitKey(1) == ord('q'):
-        break
+camera = cv2.VideoCapture(0)
 
-camera.release()
-cv2.destroyAllWindows()
+def onAppStart(app):
+    app.position = (0, 0, 0)
+
+def onStep(app):
+    app.position = capturePosition()
+
+def redrawAll(app):
+    drawCircle(app.position[0], app.position[1], 10, fill='red')
+
+def main():
+    runApp(800, 450)
+
+main()
